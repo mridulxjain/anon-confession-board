163bac6a3e6a1c5431c98cf8852e74021f3e8e07
mridulxjain <krishnajain2604@gmail.com>
2025-09-25 01:29:19 +0530

    setup and add files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Anonymous Confession Board</h1>
+    </>
+  )
+}
+
+export default App

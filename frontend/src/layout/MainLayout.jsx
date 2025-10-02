d651d89d002ef0ce5aa9609283877d97994f4a2b
mridulxjain <krishnajain2604@gmail.com>
2025-10-02 19:03:21 +0530

    fix path in layout

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import { useState } from "react";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 
 export default function MainLayout() {
   return (
